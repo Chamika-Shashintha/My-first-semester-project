26c917595e7443afe96923735933c5948ab43320
S.W.B Chamika Shashintha Bandara <chamikashashintha1008@gmail.com>
2025-02-04 12:27:19 +0530

    Add files via upload

---
+++ attraction.js
@@ -0,0 +1,15 @@
+function toggleReadMore(index) {
+    var dots = document.getElementById("dots" + index);
+    var moreText = document.getElementById("more" + index);
+    var btnText = document.getElementById("myBtn" + index);
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
